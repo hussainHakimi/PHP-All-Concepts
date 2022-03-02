47d0fa2711b9d320067bdbbab2b5b8c35664fdc9
hussainHakimi <hussainbayan614@gmail.com>
2022-03-03 04:22:38 +0430

    index.php file is added.

---
+++ index.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP All Concepts</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <style>
+        
+
+
+    </style>
+    <div id="container">
+        <div id="left_sidebar">
+            
+        <ul>
+                <span>PHP Concepts</span>
+                <a href="#">
+                    <li>
+                        PHP Introduction
+                    </li>
+                </a>
+                <a href="syntax.html">
+                    <li>
+                        PHP Syntax
+                    </li>
+                </a>
+                <a href="comments.html">
+                    <li>
+                        PHP Comments
+                    </li>
+                </a>
+                <a href="variable.html">
+                    <li>
+                        PHP Variable
+                    </li>
+                </a>
+                <a href="echo.html">
+                    <li>
+                        PHP Echo/Print
+                    </li>
+                </a>
+                <a href="data.html">
+                    <li>
+                        PHP Data Types
+                    </li>
+                </a>
+                <a href="condition.html">
+                    <li>
+                        PHP Condition
+                    </li>
+                </a>
+                <a href="loop.html">
+                    <li>
+                        PHP Loops
+                    </li>
+                </a>
+                <a href="function.html">
+                    <li>
+                    PHP Function
+                    </li>
+                </a>         
+                <a href="array.html">
+                    <li>
+                    PHP Array
+                    </li>
+                </a>
+            </ul>
+        </div>
+
+        <style>
+            
+        </style>
+        <div id="main_content">
+            <h1>PHP CONCEPTS</h1>
+            <div id="syntax">
+                <h2>PHP Introduction</h2>
+                <p>PHP code is executed on the server.</p>
+                <h3>What You Should Already Know</h3>
+                <ul>
+                    <p>Before you continue you should have a basic understanding of the following:</p>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+                <h2>What is PHP?</h2>
+                <ul>
+                    <li>PHP is an acronym for "PHP: Hypertext Preprocessor"</li>
+                    <li>PHP is a widely-used, open source scripting language</li>
+                    <li>PHP scripts are executed on the server</li>
+                    <li>PHP is free to download and use</li>
+                </ul>
+                <ul>
+                    <h4>PHP is an amazing and popular language!</h4>
+                    <li>It is powerful enough to be at the core of the biggest blogging system on the web (WordPress)!</li>
+                    <li>It is deep enough to run the largest social network (Facebook)!</li>
+                    <li>It is also easy enough to be a beginner's first server side language!</li>
+                </ul>
+                <ul>
+                    <h2>What is a PHP File?</h2>
+                    <li>PHP files can contain text, HTML, CSS, JavaScript, and PHP code</li>
+                    <li>PHP code is executed on the server, and the result is returned to the browser as plain HTML</li>
+                    <li>PHP files have extension ".php"</li>
+                </ul>
+                <ul>
+                    <h2>What Can PHP Do?</h2>
+                    <li>PHP can generate dynamic page content</li>
+                    <li>PHP can create, open, read, write, delete, and close files on the server</li>
+                    <li>PHP can collect form data</li>
+                    <li>PHP can send and receive cookies</li>
+                    <li>PHP can add, delete, modify data in your database</li>
+                    <li>PHP can be used to control user-access</li>
+                    <li>PHP can encrypt data</li>
+                </ul>
+                <p>With PHP you are not limited to output HTML. You can output images, PDF files, and even Flash movies. You can also output any text, such as XHTML and XML.</p>
+                <ul>
+                    <h2>Why PHP?</h2>
+                    <li>PHP runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)</li>
+                    <li>PHP is compatible with almost all servers used today (Apache, IIS, etc.)</li>
+                    <li>PHP supports a wide range of databases</li>
+                    <li>PHP is free. Download it from the official PHP resource: www.php.net</li>
+                    <li>PHP is easy to learn and runs efficiently on the server side</li>
+                </ul>
+                <ul>
+                    <h2>What's new in PHP 7</h2>
+                    <li>PHP 7 is much faster than the previous popular stable release (PHP 5.6)</li>
+                    <li>PHP 7 has improved Error Handling</li>
+                    <li>PHP 7 supports stricter Type Declarations for function arguments</li>
+                    <li>PHP 7 supports new operators (like the spaceship operator: <=>)</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
